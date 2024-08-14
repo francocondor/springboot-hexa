2c25dc46a9b46e3045b4078bce34c36a9eaaf35d
francobko <franco.condor@urp.edu.pe>
2024-08-13 22:08:24 -0500

    Initial commit

---
+++ src/main/java/com/loxasoft/springboot_hexa/SpringbootHexaApplication.java
@@ -0,0 +1,13 @@
+package com.loxasoft.springboot_hexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHexaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loxasoft/springboot_hexa/SpringbootHexaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loxasoft.springboot_hexa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHexaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
