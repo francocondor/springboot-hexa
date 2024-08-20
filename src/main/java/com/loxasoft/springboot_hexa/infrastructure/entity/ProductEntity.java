a8492db375247ab4a5c1f18601e060bd863c4ee4
francobko <franco.condor@urp.edu.pe>
2024-08-20 18:55:07 -0500

    feat: Add config for deploy

--- src/main/java/com/loxasoft/springboot_hexa/infrastructure/entity/ProductEntity.java
+++ src/main/java/com/loxasoft/springboot_hexa/infrastructure/entity/ProductEntity.java
@@ -6,28 +6,29 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "products")
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
+@AllArgsConstructor // para que funcione las validaciones
+@NoArgsConstructor // para que no de error al obtener el producto con el getProduct(Integer id)
+@Data //getter y setter
 public class ProductEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String code;
-    @Size(min = 4, max = 50, message = "El nombre debe tener entre 4 y 50 caracteres")
-    @NotBlank(message = "El nombre es requerido")
+    @Size(min = 4, max = 50, message = "Nombre de producto debe contener al menos 5 caracteres")
+    @NotBlank(message = "Nombre de producto no debe estar en blanco")
     private String name;
-    @NotBlank(message = "La descripción es requerida")
+    @NotBlank(message = "Descripción de producto no debe estar vacío")
     private String description;
-    private String urlImage;
-    @NotBlank(message = "El precio es requerido")
-    @Positive(message = "El precio debe ser mayor a 0")
+    private String urlImagen;
+    @NotNull(message = "Precio de producto es requerido")
+    @Positive(message = "Precio no debe ser un número negativo o cero")
     private Double price;
     private LocalDateTime dateCreated;
     private LocalDateTime dateUpdated;
